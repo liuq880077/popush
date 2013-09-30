e311f0b8917c43631cd378ebaef139eb315d4530
chenlu11 <1650888961@qq.com>
2013-09-30 16:24:15 +0800

    cookie for language choice

--- static/js/popush.js
+++ static/js/popush.js
@@ -958,17 +958,60 @@ function backto(n) {
 		currentDirString = getdirstring();
 	});
 }
+/////////////////////// addon ///////////////////////////
+function getCookie(c_name) {
+	if (document.cookie.length>0) {
+	  c_start=document.cookie.indexOf(c_name + "=");
+	  if (c_start!=-1) {
+	    c_start=c_start + c_name.length+1;
+	    c_end=document.cookie.indexOf(";",c_start);
+	    if (c_end==-1) 
+		c_end=document.cookie.length;
+	    return unescape(document.cookie.substring(c_start,c_end));
+	  } 
+	}
+	return "";
+}
+
+function loadjsfile(filename){
+	var fileref = document.createElement('script');
+	fileref.setAttribute("type","text/javascript");
+	fileref.setAttribute("src",filename); 
+}
+
+function setCookie(c_name,value,expiredays) {
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate()+expiredays);
+	document.cookie=c_name+ "=" +escape(value)+((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
+}
+
+function checkCookie() {
+	var language=getCookie('language')
+	if (language == 'cn') {
+	  strings = strings_cn;
+	}
+	else if (language == 'en') {
+	  strings = strings_en;
+	}
+	else {
+	  language=prompt('Please enter your language("cn" or "en"):',"");
+	  if (language!=null && language!="") {
+	    setCookie('language',language,365);
+	    window.location.href = "index.html";
+	  }
+	}
+}
 
 /////////////////////// initialize ///////////////////////////
 
 $(document).ready(function() {
 	setTimeout('loadfailed()', 10000);
-
-    CodeMirror.on(window, "resize", function() {
+	checkCookie();
+	CodeMirror.on(window, "resize", function() {
 		var showing = document.getElementsByClassName("CodeMirror-fullscreen")[0];
 		if (!showing) return;
 		showing.CodeMirror.getWrapperElement().style.height = winHeight() + "px";
-    });
+	});
 
 	editor = CodeMirror.fromTextArea($('#editor-textarea').get(0), {
 		lineNumbers: true,
