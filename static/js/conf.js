0e3083fda12c7c739762fe4530d3bc48e86de402
gongdh11 <gdh1995>
2013-11-03 22:44:10 +0800

    fix conf again

--- static/js/conf.js
+++ static/js/conf.js
@@ -2,9 +2,9 @@ var app = app || {};
 
 app.Package = {
   VERSION: '1.0.3',
-  /* SOCKET_IO: '/', */
+  SOCKET_IO: '/',
   /* SOCKET_IO: '10.10.10.128/', */
-  SOCKET_IO: 'http://localhost:4444/',
+  /* SOCKET_IO: 'http://localhost:4444/', */
   SAVE_TIME_OUT: 1000,
   ENABLE_RUN: true,
   ENABLE_DEBUG: true,
