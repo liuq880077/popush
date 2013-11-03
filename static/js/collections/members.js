f0a9bfc6dd8c3f40c23be295b10a60d28fd7f5f1
chenlu11 <chenlu11@mails.tsinghua.edu.cn>
2013-11-03 21:54:17 +0800

    Update members.js

--- static/js/collections/members.js
+++ static/js/collections/members.js
@@ -1,70 +1,72 @@
-/*global Backbone */
-var app = app || {};
+/* Members Collection */
+/* model: app.User */
+var app = app || {}; 
 
-(function () {
+(function() {
 	'use strict';
 
 	app.Members = Backbone.Collection.extend({
-		// Reference to this collection's model.
+
 		model: app.User,
 
-		comparator: function (member) {
-      var a = member.attributes;
+		comparator: function(member) {
+			var a = member.attributes;
 			var value = "";
 			if (!a.owner)
-				value += "1";
+			 value += "1";
 			else
-				value += "0";
+			 value += "0";
 			if (!a.online)
-				value += '1';
+			 value += '1';
 			else
-				value += "0";
+			 value += "0";
 			return value + a.name;
 		},
-		
-		update: function (data) {
+
+		update: function(data) {
 			var col = this;
 			col.reset();
 			for (var i = 0, d; i < data.length; ++i) {
 				d = data[i];
 				col.add({
-					name:   d.name,
+					name: d.name,
 					avatar: d.avatar,
 					online: d.online || false,
 					owner: d.owner || false,
 				});
 			}
 		},
-		
-		updatedoc: function (doc) {
+
+		updatedoc: function(doc) {
 			var col = this;
 			col['reset']({
-        name: doc.owner.name,
-        avatar: doc.owner.name == app.currentUser.name ? app.currentUser.avatar : doc.owner.avatar,
-        online: doc.owner.online || false,
-        owner: true,
-      });
-			for(var i=0; i<doc.members.length; i++) {
+				name: doc.owner.name,
+				avatar: doc.owner.name == app.currentUser.name ? app.currentUser.avatar: doc.owner.avatar,
+				online: doc.owner.online || false,
+				owner: true,
+			});
+			for (var i = 0; i < doc.members.length; i++) {
 				var user = doc.members[i];
 				this.add({
-          name:   user.name,
-          avatar: user.avatar,
-          online: user.online || false,
-          owner:  false,
-        });
+					name: user.name,
+					avatar: user.avatar,
+					online: user.online || false,
+					owner: false,
+				});
 			}
 		},
 	});
-  app.init || (app.init = {});
 
-  app.init.members = function() {
-    app.collections['members'] || (app.collections['members'] = new app.Members());
-  };
-  app.init.cooperators = function() {
-    app.collections['cooperators'] || (app.collections['cooperators'] = new app.Members());
-  };
-  app.init.shares = function() {
-    app.collections['shares'] || (app.collections['shares'] = new app.Members());
-  };
+	app.init || (app.init = {});
 
+	app.init.members = function() {
+		app.collections['members'] || (app.collections['members'] = new app.Members());
+	};
+	app.init.cooperators = function() {
+		app.collections['cooperators'] || (app.collections['cooperators'] = new app.Members());
+	};
+	app.init.shares = function() {
+		app.collections['shares'] || (app.collections['shares'] = new app.Members());
+	};
+	
 })();
